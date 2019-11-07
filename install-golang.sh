239b8943b4590846ffb94add7a00a900d5d2cd25
Zoltan Giber <zoltan@zoltans-iMac.local>
2019-11-07 00:46:51 +0000

    uncomment go installer

--- install-golang.sh
+++ install-golang.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-# sudo rm -rf /usr/local/go
-# sudo tar -C /usr/local -xzf go1.13.4.linux-amd64.tar.gz
-# if ! grep -Fxq 'export PATH=$PATH:/usr/local/go/bin' ~/.profile; then
-#     echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-# fi
+wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.13.4.linux-amd64.tar.gz
+if ! grep -Fxq 'export PATH=$PATH:/usr/local/go/bin' ~/.profile; then
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+fi
 mkdir -p $HOME/go/bin
 if ! grep -Fxq 'export PATH=$PATH:$HOME/go/bin' ~/.profile; then 
 	echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.profile
